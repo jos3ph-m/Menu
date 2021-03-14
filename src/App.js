f2a40fb59c3ee0cfc81f01bd48439cbfb8ad6173
Joseph M <jmedina1985@hotmail.com>
2021-03-13 18:28:35 -0800

    working on setting up buttons to work dynamically

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         <div className="title">
           <h2>our menu</h2>
           <div className="underline"></div>
-          <Categories filterItems={filterItems} />
+          <Categories categories={categories} filterItems={filterItems} />
           <Menu items={menuItems} />
         </div>
       </section>
